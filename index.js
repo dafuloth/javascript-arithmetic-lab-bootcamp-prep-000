b23847da023b1b3c0a6835ac428a631f223b364e
David Thai <david.thai@dafuloth.me>
2018-08-26 21:49:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ function dec(n) {
   return n -=1
 }
 
-function makeInt(a) {
-  return parseInt(a, 10)
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
